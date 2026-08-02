3c13464ec95e96137881f42f79ca1a2e2d05ec55
KaitoKidAdventurine <kaitokid349d@gmail.com>
2026-08-02 14:50:44 -0400

    feat: Creacion del ENUM CategoriaProducto

---
+++ Enum/CategoriaProducto.cs
@@ -0,0 +1,16 @@
+public enum CategoriaProducto
+{
+    Electronica,        // Televisores, audífonos, parlantes, cables
+    Informatica,        // Laptops, monitores, teclados, mouse, discos duros
+    Literatura,         // Libros, revistas, cómics (tanto físicos como digitales)
+    Capacitacion,       // Cursos online, talleres, tutoriales en video
+    Videojuegos,        // Juegos de PC/Consola (físicos o códigos digitales)
+    Musica,             // Instrumentos, canciones digitales, álbumes
+    Hogar,              // Muebles, utensilios de cocina, decoración
+    Deportes,           // Equipamiento deportivo, pesas, ropa deportiva
+    Oficina,            // Papelería, sillas de escritorio, tinta, cuadernos
+    Salud,              // Suplementos, equipos médicos, vitaminas
+    Ropa,               // Prendas de vestir, calzado
+    Alimentos,          // Comida envasada, bebidas (si la tienda vende)
+    Servicios           // Instalaciones, garantías extendidas o soporte técnico
+}
