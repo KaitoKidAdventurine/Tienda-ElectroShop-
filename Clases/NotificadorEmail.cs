f828cbd3615f876c701ea886be6857913247b49f
KaitoKidAdventurine <kaitokid349d@gmail.com>
2026-08-08 14:21:18 -0400

    Update NotificadorEmail.cs
    Actualizacion de el lugar del namespace

--- Clases/NotificadorEmail.cs
+++ Clases/NotificadorEmail.cs
@@ -1,8 +1,14 @@
 
-class NotificadorEmail : INotificable
+using System;
+using TiendaElectroShop.Interfaces;
+
+namespace TiendaElectroShop.Clases
+{
+    class NotificadorEmail : INotificable
 {
     public void Enviar(string destinatario, string mensaje)
     {
         Console.WriteLine($"EMAIL a {destinatario}: {mensaje}");
+        }
     }
 }
