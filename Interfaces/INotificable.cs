84f136e637106202abf4bcc07ca9acbc75b29f09
KaitoKidAdventurine <kaitokid349d@gmail.com>
2026-08-08 14:23:36 -0400

    Update INotificable.cs
    Actualizacion de el namespace

--- Interfaces/INotificable.cs
+++ Interfaces/INotificable.cs
@@ -1,20 +1,7 @@
-interface INotificable
+namespace TiendaElectroShop.Interfaces
+{
+    interface INotificable
 {
     void Enviar(string destinatario, string mensaje);
-}
-
-class NotificadorEmail : INotificable
-{
-    public void Enviar(string destinatario, string mensaje)
-    {
-        Console.WriteLine($"EMAIL a {destinatario}: {mensaje}");
-    }
-}
-
-class NotificadorSMS : INotificable
-{
-    public void Enviar(string destinatario, string mensaje)
-    {
-        Console.WriteLine($"SMS a {destinatario}: {mensaje}");
     }
 }
